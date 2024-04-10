5b5459ffac0fb09ccb70df29adc5d8382f6a465e
Gerald1702 <159792575+Gerald1702@users.noreply.github.com>
2024-04-10 10:56:58 +0000

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,12 +4,12 @@ import project1 from "./images/projects/project.1.jpg";
 import ziibo from "./images/projects/ziibo.png";
 import project17 from "./images/projects/project.17.png";
 import project20 from "./images/projects/project.20.jpg";
-import project11 from "./images/projects/project.11.JPG"
-import project15 from "./images/projects/project.15.JPG"
-import project16 from "./images/projects/project.16.JPG"
-import project09 from "./images/projects/project.09.jpg"
-import project00 from "./images/projects/project00.png"
-import project50 from "./images/projects/project.50.jpg
+import project11 from "./images/projects/project.11.JPG";
+import project15 from "./images/projects/project.15.JPG";
+import project16 from "./images/projects/project.16.JPG";
+import project09 from "./images/projects/project.09.jpg";
+import project00 from "./images/projects/project00.png";
+import project50 from "./images/projects/project.50.jpg";
 
 import contactImg from "./images/contactImg.jpg";
 import quote from "./images/quote.png";
